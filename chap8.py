6c7470ac02cd3b2d36b8c750037279385a765058
risa <lisaffang@gmail.com>
2014-06-26 13:23:34 -0400

    book exercises

--- chap8.py
+++ chap8.py
@@ -1,2 +1,19 @@
 # This is where the answers to Chapter 8 questions for the BSS Dev RampUp go
-# Name:
+# Name: Lisa
+
+def rotate_letter(letter, n):
+	if letter.isupper():
+		start = ord('A')		# returns Unicode integer that corresponds with letter
+	elif letter.islower():
+		start = ord('a')
+	else:
+		return letter
+	c = ord(letter) - start		# calculations... don't understand 
+	i = (c + n) % 26 + start
+	return chr(i)				# turns the Unicode into a string again
+
+def rotate_word(string, n):
+	result = ''
+	for letter in string:		# why does this just stop when letters end instead of erroring?
+		result += rotate_letter(letter, n)
+	return result
--- chap9.py
+++ chap9.py
@@ -1,2 +1,51 @@
 # This is where the answers to Chapter 9 questions for the BSS Dev RampUp go
-# Name:
+# Name: Lisa
+
+""" 
+Exercise 9.1. Write a program that reads words.txt and prints only the words with more than 20 characters (not counting whitespace).
+
+Exercise 9.2. Write a function called has_no_e that returns True if the given word doesn’t have the letter “e” in it.
+Modify your program from the previous section to print only the words that have no “e” and com- pute the percentage of the words in the list have no “e.”
+
+Exercise 9.3. Write a function named avoids that takes a word and a string of forbidden letters, and that returns True if the word doesn’t use any of the forbidden letters.
+Modify your program to prompt the user to enter a string of forbidden letters and then print the number of words that don’t contain any of them. Can you find a combination of 5 forbidden letters that excludes the smallest number of words?
+
+Exercise 9.4. Write a function named uses_only that takes a word and a string of letters, and that returns True if the word contains only letters in the list. Can you make a sentence using only the letters acefhlo? Other than “Hoe alfalfa?”
+"""
+
+# 9.1
+
+def long_words():
+	fin = open(words.txt)
+	for line in fin:
+		word = line.strip()
+		if len.word >= 20:
+			print word
+		else:
+			long_words
+# 9.2
+
+def has_no_e(word):
+	for letter in word:
+		if letter == 'e':
+			return False
+	return True
+
+# 9.3
+
+def avoids(word, string):
+	for letter in word:
+		if letter in string:
+			return False
+	return True
+
+# 9.4
+
+def uses_only(word, string):
+	for letter in word:
+		if letter not in string:
+			return False
+	return True
+
+#  Did not address: Can you make a sentence using only the letters acefhlo? Other than “Hoe alfalfa?” 
+
